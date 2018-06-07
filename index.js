6c8c4345133321a5de042dac002521c8d0956c88
khuchuan <khuchuan@gmail.com>
2018-06-08 05:17:18 +0700

    update

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3000
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Hi everybody!');
+res.end('Xin chao cac ban.');
 });
 server.listen(PORT);
